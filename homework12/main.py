84bf7e67f7605dd15ddb7d7a1918b5ef78ad976e
VoixKz <karimtaevbeksultan@gmail.com>
2024-07-16 11:51:55 +0500

    done 12th hw

---
+++ homework12/main.py
@@ -0,0 +1,9 @@
+#git init - иницализируем
+#git remote add origin "ссылка на репозиторий" - добавляем удаленный репозиторий если надо
+#git pull origin main(or another branch) - тянем изменения с репозитория
+#git add . - добавляем все файлы
+#git commit -m "English or Spanish?" - делаем коммит
+#git push origin main(or another branch) - пушим в репозиторий
+
+#Jira 
+#https://beksultanardakov.atlassian.net/jira/software/projects/KAN/boards/1/timeline?shared=&atlOrigin=eyJpIjoiZWY4OGY2ZWI0ODcwNDU2NGJhMmM1ZGI5YmY2ZmM2OWEiLCJwIjoiaiJ9
